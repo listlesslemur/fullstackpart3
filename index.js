c517e20163e6c437b6adc51e72d8ab318d2c8a26
Daryl Patino <listlesslemur@gmail.com>
2025-02-17 13:51:45 -0600

    dd

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    }
-]
-
-const generateId = () => {
-    const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-
-    return String(maxId + 1)
-}
-
-const requestLogger = (request, response, next) => {
-    console.log('Method: ', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ', request.body)
-    console.log('---')
-    next()
-}
+const name = process.argv[2]
+const number = process.argv[3]
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -44,16 +15,12 @@ morgan.token('body', request => {
     return JSON.stringify(request.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-//app.use(requestLogger)
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request,response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number
+})
+
+if (name) {
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+else {
+    Person.find({}).then(results => {
+        results.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
